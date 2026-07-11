8fb09d94202177e500d03a7dece081fa6252d50d
perezlindoignacio8-hash <perezlindoignacio8@gmail.com>
2026-07-10 23:06:56 -0300

    .

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -26,7 +26,7 @@ export default function MetaPixel() {
     const script = document.createElement('script');
     script.innerHTML = `
       !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '1713785829975280');
+      fbq('init', '27452045544446714');
       fbq('track', 'PageView');
     `;
     script.async = true;
@@ -36,7 +36,7 @@ export default function MetaPixel() {
     const img = document.createElement('img');
     img.height = 1;
     img.width = 1;
-    img.src = 'https://www.facebook.com/tr?id=1713785829975280&ev=PageView&noscript=1';
+    img.src = 'https://www.facebook.com/tr?id=27452045544446714&ev=PageView&noscript=1';
     img.style.display = 'none';
     noscript.appendChild(img);
     document.head.appendChild(noscript);
